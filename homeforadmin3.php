d7531fa488f8051ec8cb94b0b824214e46cde26b
unknown <surajshinde7077@gmail.com>
2020-08-30 10:08:30 +0530

    added

---
+++ homeforadmin3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body style="background: -webkit-linear-gradient(left,#5DADE2,#00c6ff);">
+
+<div class=" bg-dark " style="width: 500px;margin-top: 15%;margin-left:32%;border-radius: 10px;">
+	<div>
+		<div>
+			<h1 class="text-center py-5 "style="color: Tomato;">Welcome Admin</h1>
+		</div>
+	</div>
+	<div class="container">
+	  	<div class="row">
+	  		<a href="homeforadmin.php" class="btn btn-info mx-auto mb-5" role="button">Add Question & Answer </a>
+	  	    <a href="homeforadmin2.php" class="btn btn-info mx-auto mb-5" role="button">Display data.</a>
+	  	    <a href="mainhome.php" class="btn btn-info mx-auto mb-5" role="button">logout.</a>
+	  	</div>
+	</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ mainhome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	
+</head>
+<body>
+<body background="Quiz-bg.jpg">
+
+<div class=" bg-dark " style="width: 500px;margin-top: 15%;margin-left:32%;border-radius: 10px;">
+	<div>
+		<div>
+			<h1 class="text-center py-5 "style="color: Tomato;">Welcome to QuizWorld</h1>
+		</div>
+	</div>
+	<div class="container">
+	  	<div class="row">
+	  		<a href="loginforadmin.php" class="btn btn-info mx-auto mb-5" role="button">Admin</a>
+	  	    <a href="login.php" class="btn btn-info mx-auto mb-5" role="button">Students</a>
+	  	</div>
+	</div>
+</div>
+	
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
